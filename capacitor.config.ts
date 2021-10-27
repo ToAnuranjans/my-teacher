c2b168fc831d28226a0646d2e92a19327145870d
Anuranjan <>
2021-10-27 17:17:04 +0530

    rebuild for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,18 @@
+/// <reference types="@capacitor/keyboard" />
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.anuranjans.teachers',
   appName: 'My Teacher',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    keyboard: {
+      resize: 'body',
+      style: 'dark',
+      resizeOnFullScreen: true,
+    },
+  },
 };
 
 export default config;
